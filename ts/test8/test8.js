af3f523d88b5a9129ed0889b6461d2ad8c72ab58
{krism01} <{krism01@naver.com}>
2022-08-03 15:42:12 +0900

    ts function 정리

--- ts/test8/test8.js
+++ ts/test8/test8.js
@@ -1,3 +1,16 @@
-// function 가위바위보(char: '가위'|'바위'|'보'): string[] {
-//   return char;
-// }
+let cutZero = (a) => {
+    let newArr = Array.from(a);
+    let answer = '';
+    newArr[0] === '0'
+        ? answer = newArr.slice(1).join('')
+        : null;
+    // console.log('answer: ', answer);
+    return answer;
+};
+let removeDash = (b) => {
+    let answer = +(b.split('-').join(''));
+    // console.log('answer: ', answer);
+    return answer;
+};
+cutZero('0mjh');
+removeDash('010-7339-6954');
