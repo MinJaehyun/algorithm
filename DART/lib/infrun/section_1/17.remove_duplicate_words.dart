01c8b160aa8838606cdbdcd42e904c9d64c0b3a2
{krism01} <{krism01@naver.com}>
2023-01-23 13:48:46 +0900

    toSet() 사용하지 않고 다르게 구현해보기

--- DART/lib/infrun/section_1/17.remove_duplicate_words.dart
+++ DART/lib/infrun/section_1/17.remove_duplicate_words.dart
@@ -1,26 +1,15 @@
-import 'package:flutter/material.dart';
+void main() {
+  List<String> words = ['good', 'time', 'good', 'time', 'student'];
+  Set<String> newWords = words.toSet();
+  print(newWords.toList()); // [good, time, student]
 
-class DuplicateWords extends StatefulWidget {
-  const DuplicateWords({Key? key}) : super(key: key);
-
-  @override
-  State<DuplicateWords> createState() => _DuplicateWordsState();
-}
-
-class _DuplicateWordsState extends State<DuplicateWords> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        child: Column(
-          children: [
-            Text('test'),
-          ],
-        ),
-      ),
-    );
-  }
+  // print(words.asMap()); // {0: good, 1: time, 2: good, 3: time, 4: student}
 }
 
 /**
+ * note: where 는 index 값을 제공해주지 않는다.
+ * note: where은 return 값이 배열이므로, 기존 배열을 수정하길 원하면 retainWhere을 사용한다.
+ *
+ * note: List 에서 index 를 받고 싶은 경우는 ?
+ * - asMap 사용한다
  * */
